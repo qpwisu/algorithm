2fb082fd5ad54c0b673af0e083d663c555f83ca0
qpwisu <qpwisu@gmail.com>
2022-01-13 10:23:13 +0900

    easy

---
+++ 프로그래머스/스택,큐/주식가격.py
@@ -0,0 +1,13 @@
+#매우 쉬움
+def solution(prices):
+    answer = []
+    for i in range(len(prices)-1):
+        count = 0
+        for j in range(i+1,len(prices)):
+            count+=1
+            if prices[i]>prices[j]:
+                
+                break
+        answer.append(count)
+    answer.append(0)
+    return answer
