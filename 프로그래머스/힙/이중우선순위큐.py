9f42c49bed3bd8be0de61097f420f46320ed614a
qpwisu <qpwisu@gmail.com>
2022-01-16 15:42:53 +0900

    heapq활용

--- 프로그래머스/힙/이중우선순위큐.py
+++ 프로그래머스/힙/이중우선순위큐.py
@@ -1,6 +1,7 @@
 import heapq
 def solution(operations):
     queue=[]
+    
     while operations: 
         q= operations.pop(0).split(" ")
         if q[0]=="I":
@@ -8,7 +9,7 @@ def solution(operations):
         elif q[0]=="D" and q[1]=="1":
             if len(queue)==0:
                 continue
-            queue.pop() #pop은 heappush 처럼 가장 작은 값을 가져오진않지만 기존에 heapush로 정렬된 힙에서 가장 끝의 수는 최대값을 보장함으로 pop을 사용해도된다 
+            queue.remove(max(queue))  #가장 큰수를 뽑아서 제거 
         elif q[0]=="D" and q[1]=="-1":
             if len(queue)==0:
                 continue
