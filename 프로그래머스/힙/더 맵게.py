9e1e40d78acbffa19927428902381fd24bb73c89
qpwisu <qpwisu@gmail.com>
2022-01-16 09:48:51 +0900

    heapq 사용법 다시보기

---
+++ 프로그래머스/힙/더 맵게.py
@@ -0,0 +1,34 @@
+#heapq 사용 하는 방법을 알아야 푼다. 리스트로 매번 정렬하면서 풀면 효율성 오류 
+#heapq 작은 수 부터 자동으로 정렬하여 저장한다. 반대로 하면 우선순위가 함께든 튜플로 저장하여야 한다. 
+import heapq 
+def solution(scoville, K):
+    heapq.heapify(scoville) #리스트를 heap으로 변경 근데 이걸 다른 변수로 저장하면 null이 뜬다 why?
+    answer = 0
+    while len(scoville)>=2:
+        a= heapq.heappop(scoville) #맨앞 가장 작은 수를 pop한다 
+        b=heapq.heappop(scoville)
+        if a<K:
+            mix= a+(b*2)
+            heapq.heappush(scoville,mix) #heap에 mix 추가
+            answer+=1
+            if len(scoville)==1 and scoville[0]<K:
+                return -1
+        else:
+            break
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
