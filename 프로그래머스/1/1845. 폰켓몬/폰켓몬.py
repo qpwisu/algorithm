cb8b96fb6e7bdb2a156da750db973edb625549a1
qpwisu <qpwisu@kookmin.ac.kr>
2024-08-07 23:38:01 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,41 @@
 def solution(nums):
     answer = 0
-    set_nums = set(nums)
-    print(set_nums)
-    answer = len(set_nums)  
-    if answer > len(nums)/2:
-        answer = int(len(nums)/2)
+
+    n = int(len(nums)/2)
+    
+    l = len(set(nums))
+    print(n,l)
+    
+    if n>l:
+        answer = l
+    else:
+        answer = n
+    
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(nums):
+#     answer = 0
+#     set_nums = set(nums)
+#     print(set_nums)
+#     answer = len(set_nums)  
+#     if answer > len(nums)/2:
+#         answer = int(len(nums)/2)
+#     return answer
