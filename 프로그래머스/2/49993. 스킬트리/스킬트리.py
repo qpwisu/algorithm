b6a81c7cdf3542c7df874dc8f6ddf25f0a22072e
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-08 03:53:33 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,26 +1,15 @@
-from itertools  import combinations
+
 def solution(skill, skill_trees):
-    skill_li = list(skill)
-    
-    tmp_li = []
-    
-    for i in range(len(skill)):
-        tmp_li.append(skill[:i+1])
-    
-    print(tmp_li)
-    
-    li = [ list(s) for s in skill_trees]
     answer = 0
 
-    for j in range(len(li)):
-        tmp = ""
-        for i in range(len(li[j])):
-            if li[j][i] in skill:
-                tmp+=li[j][i]
-        if tmp in tmp_li:
-            answer +=1 
-        if tmp == "":
-            answer +=1 
+    for skills in skill_trees:
+        skill_list = list(skill)
 
-            
+        for s in skills:
+            if s in skill:
+                if s != skill_list.pop(0):
+                    break
+        else:
+            answer += 1
+
     return answer
