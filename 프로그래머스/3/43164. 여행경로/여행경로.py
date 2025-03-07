72a80640d0af46ab8ce4e669ad1ecbfccf13c03f
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-08 03:11:34 +0900

    [level 3] Title: 여행경로, Time: 694.99 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,69 +1,28 @@
-
+from collections import deque
 def solution(tickets):
-    li = []
-    N = len(tickets)
+    answer = ["ICN"]
+    n = len(tickets) 
+    
+    tickets.sort()
+    queue = deque([("ICN",[],0)]) #시작 공항, visited, 이동 횟수 
+    visited = []
+    
+    while queue:
+        start, visit, count = queue.popleft()
+        
+        if count == n:
+            visited = visit
+            break
+            
+        for i in range(n):
+            if i in visit:
+                continue 
+            
+            if start == tickets[i][0]:
+                queue.append((tickets[i][1],visit+[i],count+1))
+        
 
-    def  dfs(t, visited):
-        visited = visited +[t]
-        if N == len(visited):
-            li.append(visited)
-            return 
-        
-        start= tickets[t][0]
-        end = tickets[t][1]
-        
-        for t in range(len(tickets)):
-            if t in visited:
-                continue
-            if end == tickets[t][0]:
-                dfs(t,visited)
-        
-    for t in range(len(tickets)):
-        if tickets[t][0] == 'ICN':
-            dfs(t,[])
-    answer = []
-    for l in li:
-        if N+1 == len(l):
-            continue
-        tmp = ["ICN"]
-        for t in range(len(l)):
-            tmp.append(tickets[l[t]][1])
-        answer.append(tmp)
-        
-    answer = sorted(answer)
-    answer=answer[0]
+    for i in range(n):
+        answer.append(tickets[visited[i]][1])
+
     return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from collections import defaultdict
-# def solution(tickets):
-#     r = defaultdict(list) 
-#     #defaultdict로 초기값을 지정해준다 
-#     for i,j in tickets:
-#         r[i].append(j)
-#     for i in r.keys():
-#         r[i].sort()
-
-#     s = ["ICN"] #스택
-#     p = []
-#     while s:
-#         q = s[-1]
-#         if r[q] != []:#스택의 top에서 이동경로가 있는경우 스택에 맨앞 경로를 추가 
-#             s.append(r[q].pop(0))
-#         else: #도착 공항이 없는 경우만 p에 추가 
-#             p.append(s.pop())
-#     return p[::-1] #역순
