772e79d80aa3fc8b33dabbef287aeb7ee6e3700f
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-25 20:23:16 +0900

    [level 1] Title: 유연근무제, Time: 5.80 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 11.76 ms
+메모리: 9.82 MB, 시간: 5.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 20:19:21
+2025년 03월 25일 20:23:14
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,42 +1,54 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+#16:13
 def solution(schedules, timelogs, startday):
-    li =[]
-    
-    for i in range(7):
-        if startday == 6:
-            startday +=1 
-            continue
-        elif startday == 7:
-            startday = 0
-            continue
-        startday +=1 
-        li.append(i)
-        
-    timelogs =  [[t[i] for i in range(7) if i in li]for t in timelogs]
-    
-    answer = 0
-    
-    for s in range(len(schedules)):
-        count = 0
-        
-        sh = schedules[s]//100
-        sm = schedules[s]%100 +10
-        
-        if sm >=60:
-            sh +=1 
-            sm = sm % 60
-        
-        for i in range(5):
-            th = timelogs[s][i]//100
-            tm = timelogs[s][i]%100 
-            print(th,tm)
+    answer = len(schedules)
 
-            if th < sh:
-                count +=1
-            elif th == sh:
-                if tm <= sm:
-                    count +=1
-        if count == 5:
-            answer +=1 
+    for i, time in enumerate(timelogs):
+        limit=schedules[i]+10
+        if limit%100>59: 
+            limit+=100
+            limit-=60
 
-            
+        day=startday
+        print(limit)
+        for j in time:
+            print(day)
+
+            if (day!=6 and day!=7) and j>limit:     #괄호에 or가 아니라 and
+                answer-=1
+                break
+
+            day%=7     #나머지 연산
+            day+=1
+
     return answer
+# 단, 토요일, 일요일의 출근 시각은 이벤트에 영향을 끼치지 않습니다. 
+#1. 각 사람마다 마지노 선 알아야 함
+#2. 토, 일인 인덱스 알아야 함.
