080b643990f95602a0986ef08fae5b15e5c37607
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-08-06 05:23:07 +0900

    [Gold V] Title: 암호 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,27 +1,21 @@
 L, C = map(int, input().split())
-li = sorted(input().split())
+alphabet = sorted(input().split())
+visited = [0] * C
+res = []
 
-answer = []
-aei_set = {"a", "e", "i", "o", "u"}
-
-def dfs(node_id, path, a, b):
-    if node_id == C:
-        if a >= 1 and b >= 2 and len(path) == L:
-            answer.append("".join(path))
+def dfs(depth, idx):
+    if depth == L:
+        v = sum(1 for ch in res if ch in 'aeiou')
+        c = L - v
+        if v >= 1 and c >= 2:
+            print(''.join(res))
         return
+    for i in range(idx, C):
+        if not visited[i]:
+            visited[i] = 1
+            res.append(alphabet[i])
+            dfs(depth + 1, i + 1)
+            visited[i] = 0
+            res.pop()
 
-    node = li[node_id]
-
-    # 안 넣기
-    dfs(node_id + 1, path, a, b)
-
-    # 넣기
-    if node in aei_set:
-        dfs(node_id + 1, path + [node], a + 1, b)
-    else:
-        dfs(node_id + 1, path + [node], a, b + 1)
-
-dfs(0, [], 0, 0)
-
-for p in sorted(answer):
-    print(p)
+dfs(0, 0)
