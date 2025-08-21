8d59b3f1e8bad0cc32f501205d8960351def95e3
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-08-21 17:42:53 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,75 +1,38 @@
 import java.util.*;
-class Node{
-    String word;
-    int depth;
-    int[] visited;
-    
-    Node(String word, int depth, int[] visited) {
-        this.word = word;
-        this.depth = depth;
-        this.visited = visited.clone(); // 배열 복사 (원본 안 망가뜨리기)
-    }
-}
-
 
 class Solution {
-    
-
-    private boolean compare(String word, String word2){
-        int count = 0;
-        
-        for (int i =0; i<word.length();i++){
-            
-            if (word.charAt(i)!=word2.charAt(i)){
-                count+=1; 
-                
-            }
+    private boolean canConvert(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i) && ++diff > 1) return false;
         }
-        
-        if (count != 1) return false;
-        
-        return true;
+        return diff == 1;
     }
+
     public int solution(String begin, String target, String[] words) {
-        
-        int answer = 0;
-        Queue<Node> q = new LinkedList<>();
-        int[] visited = new int[words.length];
-        Node node = new Node(begin,0,visited);
-        q.offer(node);
-        
-        outer:
-        while(!q.isEmpty()){
-            Node now = q.poll();
-            String word = now.word;
-            System.out.println(word);
-            
-            for (int i =0; i< words.length; i++){
-                 if (visited[i] == 1){
-                     continue;
-                 }
-                
-                boolean cp = compare(word, words[i]);
-                
-                if (cp){
-                    
-                    if (target.equals(words[i])){
-                        answer = now.depth+1;
-                        System.out.println(Arrays.toString(visited));
-                        return answer;
-                    }
-                    
-                    visited[i] = 1; 
-                    Node node2 = new Node(words[i], now.depth+1 ,visited);
-                    q.offer(node2);
-                    
+        boolean exist = false;
+        for (String w : words) if (w.equals(target)) { exist = true; break; }
+        if (!exist) return 0;
+
+        boolean[] visited = new boolean[words.length];
+        Queue<int[]> q = new LinkedList<>(); // [index, depth], begin은 -1
+        q.offer(new int[]{-1, 0});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            String curWord = cur[0] == -1 ? begin : words[cur[0]];
+            int depth = cur[1];
+
+            if (curWord.equals(target)) return depth;
+
+            for (int i = 0; i < words.length; i++) {
+                if (visited[i]) continue;
+                if (canConvert(curWord, words[i])) {
+                    visited[i] = true;
+                    q.offer(new int[]{i, depth + 1});
                 }
-                // word랑 비교해서 변환가능한지 확인하는 함수 
-                
             }
-            
         }
-        
-        return answer;
+        return 0;
     }
 }
