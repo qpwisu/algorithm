692cdd1eb720522236c197fea33e61feee25c9bc
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-08-29 13:42:37 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.13 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,15 +1,42 @@
-import math
+# 유클리드 거리 
+import math 
 def solution(n, w, num):
     h = math.ceil(n/w)
-    li = [2*w*i+1 for i in range(1,h)] # 밑층과 위층의 합은 2*w*층수 +1 이다 이를 통해 위층의 값을 알아 낼수 있음 
-    nh = math.ceil(num/w) -1
+    graph = [[False] * w for _ in range(h) ]
+    target = [-1,-1] 
     answer = 1
-
-    m = num
+    count = 1
     
-    for i in range(nh,h-1):
-        if li[i] - num <= n:
-            num = li[i] - num
-            answer +=1 
+    for i in range(h-1,-1,-1):
+        tmp = math.ceil(count/w) %2 
+        
+        if tmp == 1 :
+            for j in range(w):
+                if count > n:
+                    break
+                if num == count:
+                    target = [i,j]
+                
+                if i < target[0] and j == target[1]:
+                    answer += 1
+                    
+                count += 1 
+                graph[i][j] = True 
+                
             
+        if tmp == 0 :
+            for j in range(w-1, -1 ,-1):
+                if count > n:
+                    break
+                
+                if num == count:
+                    target = [i,j]
+                
+                if i < target[0] and j == target[1]:
+                    answer += 1
+                    
+                count += 1 
+                graph[i][j] = True 
+            
+    print(graph)
     return answer
