16008eb0846c0f5250efebcf5f8e1b414f3ac7c9
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-09-26 18:52:19 +0900

    [level 3] Title: 입국심사, Time: 504.07 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,19 +1,26 @@
+# 시간을 기준으로 이분탐색, 해당 시간에 각 심사대가 처리할수 있는 인원 구하기 
+# 그 인원수가 n명 이상이면 right = mid - 1 n명 이하면 left = mid +1
 def solution(n, times):
-    left, right = 1, max(times) * n
-    answer = right
+    right = max(times)* n
+    left = 1
+    answer = 0
+    while left <= right: 
+        people = 0
+        mid =(left+right) //2
 
-    while left <= right:
-        mid = (left + right) // 2
-        total = 0
         for t in times:
-            total += mid // t
-            if total >= n:  # 가지치기(조기 종료)
+            people += (mid//t)
+            if people >= n:  # 가지치기(조기 종료)
                 break
 
-        if total >= n:        # mid 시간에 n명 이상 처리 가능 → 더 줄여보기
-            answer = mid
+        
+        if people >= n:
             right = mid - 1
-        else:                 # 처리 불가 → 시간 늘리기
-            left = mid + 1
+            answer = mid
+        elif people < n:
+            left = mid +1
+            
+      
 
+    
     return answer
