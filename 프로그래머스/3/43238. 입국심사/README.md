bb9f5a4049fb3c3442cd734b70b46103df4cf3b3
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-10-17 16:54:26 +0900

    [level 3] Title: 입국심사, Time: 509.67 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
 
 ### 성능 요약
 
-메모리: 13.3 MB, 시간: 504.07 ms
+메모리: 13.5 MB, 시간: 509.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 18:52:16
+2025년 10월 17일 16:54:24
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,26 +1,22 @@
-# 시간을 기준으로 이분탐색, 해당 시간에 각 심사대가 처리할수 있는 인원 구하기 
-# 그 인원수가 n명 이상이면 right = mid - 1 n명 이하면 left = mid +1
 def solution(n, times):
-    right = max(times)* n
-    left = 1
     answer = 0
-    while left <= right: 
-        people = 0
-        mid =(left+right) //2
-
-        for t in times:
-            people += (mid//t)
-            if people >= n:  # 가지치기(조기 종료)
-                break
-
-        
-        if people >= n:
-            right = mid - 1
-            answer = mid
-        elif people < n:
-            left = mid +1
-            
-      
-
+    right = max(times) * n 
+    left = 1 
     
+    while left <= right:
+        mid = (right+left)//2
+        count = 0 
+        
+        for time in times:
+            count += (mid // time)
+            if count > n:
+                break 
+        
+        if count >= n:
+            right = mid -1 
+        else:
+            left = mid +1 
+        
+    answer = left
+        
     return answer
